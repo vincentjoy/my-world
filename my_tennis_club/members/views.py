898206b432ef46a9c0d618e26c74e2c3b3aec4fc
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-15 15:38:41 +0530

    created the first view celled members

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+"""Create your views here"""
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def members(request):
+    # The name of the view does not have to be the same as the application.
+    # I call it members because I think it fits well in this context.
+    return HttpResponse("Hello World")
