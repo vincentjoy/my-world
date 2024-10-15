292ba0b5b84415c3b6f06becd03610bc7110cae6
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-15 18:20:48 +0530

    created the details page

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members, name='members')
+    path('members/', views.members, name='members'),
+    path('members/details/<int:id>', views.details, name='details'),
 ]
