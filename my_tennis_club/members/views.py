9f49a3e6a4e09ca9df06a8afd46d64cb69e8aabe
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-15 16:25:13 +0530

    response is sent as template instead of plain text response

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -2,8 +2,10 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def members(request):
     # The name of the view does not have to be the same as the application.
     # I call it members because I think it fits well in this context.
-    return HttpResponse("Hello World")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
