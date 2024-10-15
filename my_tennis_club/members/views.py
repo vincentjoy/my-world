5bb485f219ae744061f0b0932fe83d9dc0a24f8e
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-15 18:04:22 +0530

    prepared template and view

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -3,9 +3,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
     # The name of the view does not have to be the same as the application.
     # I call it members because I think it fits well in this context.
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers
+    }
+    return HttpResponse(template.render(context, request))
