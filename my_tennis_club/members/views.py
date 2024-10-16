74f7bb7fd14cb03609ccb7499819f5c0cbb50e99
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-16 14:10:06 +0530

    played around with django variables in the template view

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -28,8 +28,11 @@ def main(request):
     return HttpResponse(template.render())
 
 def testing(request):
+    mymembers = Member.objects.all().values()
     template = loader.get_template('template.html')
     context = {
         'fruits': ['Apple', 'Banana', 'Cherry'],
+        'firstname': 'Vincent',
+        'mymembers': mymembers,
     }
     return HttpResponse(template.render(context, request))
