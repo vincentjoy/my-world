c14be24edeb44e8c7d45fd43392c15de685aa4a8
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-16 13:31:13 +0530

    created the 404 template file

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#f7@)p1lz9me&ob_^t6imkrxi(o*qd-4=cc_5h$&^zcbct2c^i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
